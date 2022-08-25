ad3bba89f5dd7efe0c423b8a44edf27a72909040
tayssir <ayyaritayssir8@gmail.com>
2022-08-25 10:59:09 +0200

    Send Email Using SpringBoot

---
+++ SpringBootEmail-master/src/main/java/com/example/springemaildemo/SpringEmailDemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.springemaildemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import javax.mail.MessagingException;
+
+@SpringBootApplication
+public class SpringEmailDemoApplication {
+
+	@Autowired
+	private EmailSenderService senderService;
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmailDemoApplication.class, args);
+	}
+	@EventListener(ApplicationReadyEvent.class)
+	public void triggerMail() throws MessagingException {
+		senderService.sendSimpleEmail("an email",
+				"This is email body",
+				"This is email subject");
+
+	}
+}
